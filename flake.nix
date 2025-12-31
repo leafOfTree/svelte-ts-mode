d7e204300849a7c00ac03ded9301cae29e758aab
meowking <mr.meowking@posteo.com>
2025-12-31 16:35:35 +0800

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakelight.url = "github:nix-community/flakelight";
+  };
+  
+  outputs = { flakelight, ... }@inputs:
+    flakelight ./. {
+      devShell = pkgs: {
+        packages = with pkgs; [
+          emacs-pgtk
+        ];
+      };
+    };
+}
